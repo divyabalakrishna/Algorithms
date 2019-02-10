9a7bcc11acc6691ce2fe1f13decc96ad0d205abd
Divya Balakrishna <balakrishnadivy@gmail.com>
2019-02-09 18:48:21 -0800

    add insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,15 +2,17 @@
 
 def insertion_sort(arr):
     n = len(arr)
-    for i in range(1, n):
-        temp = arr[i]
-        for j in range(i):
-            if temp < arr[j]:
-                del arr[i]
-                arr.insert(j, temp)
-                break
 
-arr = [1, 7, 9, 1, 4, 2, 10]
+    for j in range(1, n):
+        i = j - 1
+        key = arr[j]
+        while key < arr[i] and i >= 0:
+            arr[i+1] = arr[i]
+            i = i - 1
+        arr[i+1] = key
+    return arr
+            
+arr = [11, 7, 9, 1, 4, 2, 1, 10]
 
 insertion_sort(arr)
 print(arr)
